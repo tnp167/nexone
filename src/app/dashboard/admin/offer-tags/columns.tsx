fda80df0d932b5729c6194d991ecc9fe44e0ff0d
tnp167 <tnp_167@outlook.com>
2025-02-28 15:38:37 +0000

    Add offer tag data table

---
+++ src/app/dashboard/admin/offer-tags/columns.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import { FC, useState } from "react";
+import { useRouter } from "next/navigation";
+import CustomModal from "@/components/dashboard/Shared/CustomModal";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { useToast } from "@/hooks/use-toast";
+import { useModal } from "@/providers/modal-provider";
+
+import { Edit, MoreHorizontal, Trash } from "lucide-react";
+import { getOfferTag } from "@/queries/offer-tag";
+import { ColumnDef } from "@tanstack/react-table";
+import { OfferTag } from "@prisma/client";
+import OfferTagDetails from "@/components/dashboard/Forms/OfferTagDetails";
+import { deleteOfferTag } from "@/queries/offer-tag";
+
+export const columns: ColumnDef<OfferTag>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => {
+      return (
+        <span className="font-extrabold text-lg capitalize">
+          {row.original.name}
+        </span>
+      );
+    },
+  },
+
+  {
+    accessorKey: "url",
+    header: "URL",
+    cell: ({ row }) => {
+      return <span>/{row.original.url}</span>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const rowData = row.original;
+
+      return <CellActions rowData={rowData} />;
+    },
+  },
+];
+
+// Define props interface for CellActions component
+interface CellActionsProps {
+  rowData: OfferTag;
+}
+
+// CellActions component definition
+const CellActions: FC<CellActionsProps> = ({ rowData }) => {
+  // Hooks
+  const { setOpen, setClose } = useModal();
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+
+  // Return null if rowData or rowData.id don't exist
+  if (!rowData || !rowData.id) return null;
+
+  return (
+    <AlertDialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem
+            className="flex gap-2"
+            onClick={() => {
+              setOpen(
+                // Custom modal component
+                <CustomModal>
+                  {/* Store details component */}
+                  <OfferTagDetails data={{ ...rowData }} />
+                </CustomModal>,
+                async () => {
+                  return {
+                    rowData: await getOfferTag(rowData?.id),
+                  };
+                }
+              );
+            }}
+          >
+            <Edit size={15} />
+            Edit Details
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <AlertDialogTrigger asChild>
+            <DropdownMenuItem className="flex gap-2" onClick={() => {}}>
+              <Trash size={15} /> Delete offer tag
+            </DropdownMenuItem>
+          </AlertDialogTrigger>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AlertDialogContent className="max-w-lg">
+        <AlertDialogHeader>
+          <AlertDialogTitle className="text-left">
+            Are you absolutely sure?
+          </AlertDialogTitle>
+          <AlertDialogDescription className="text-left">
+            This action cannot be undone. This will permanently delete the
+            category and related data.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter className="flex items-center">
+          <AlertDialogCancel className="mb-2">Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={loading}
+            className="bg-destructive hover:bg-destructive mb-2 text-white"
+            onClick={async () => {
+              setLoading(true);
+              await deleteOfferTag(rowData.id);
+              toast({
+                title: "Deleted offer tag",
+                description: "The offer tag has been deleted.",
+              });
+              setLoading(false);
+              router.refresh();
+              setClose();
+            }}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
--- src/app/dashboard/admin/offer-tags/page.tsx
+++ src/app/dashboard/admin/offer-tags/page.tsx
@@ -1,7 +1,26 @@
+import DataTable from "@/components/ui/data-table";
 import React from "react";
-
-const page = () => {
-  return <div>page</div>;
+import { columns } from "./columns";
+import { Plus } from "lucide-react";
+import OfferTagDetails from "@/components/dashboard/Forms/OfferTagDetails";
+import { getAllOfferTags } from "@/queries/offer-tag";
+const page = async () => {
+  const offerTags = await getAllOfferTags();
+  return (
+    <DataTable
+      actionButtonText={
+        <>
+          <Plus />
+          Add Offer Tag
+        </>
+      }
+      modalChildren={<OfferTagDetails />}
+      columns={columns}
+      data={offerTags}
+      filterValue={"name"}
+      searchPlaceholder={"Search offer tags"}
+    />
+  );
 };
 
 export default page;
